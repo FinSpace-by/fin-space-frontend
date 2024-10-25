537e429cb5baff097e6012590f6a83cff54f0703
motfisi <108813531+motfisi@users.noreply.github.com>
2024-10-25 13:07:41 +0300

    payments

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,9 +1,21 @@
 import React, { useState } from 'react';
 import './sass/index.scss';
-import { Typography } from '@mui/material';
+import {
+  Typography,
+  CircularProgress,
+  Button,
+  Select,
+  MenuItem,
+  TextField,
+} from '@mui/material';
 
 function Cards() {
   const [activeCard, setActiveCard] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [fromCard, setFromCard] = useState('');
+  const [toCard, setToCard] = useState('');
+  const [amount, setAmount] = useState('');
 
   const handleClick = (index) => {
     setActiveCard(index);
@@ -38,37 +50,115 @@ function Cards() {
 
   return (
     <>
-      <div className="cards__container">
-        {cardData.map((card, index) => (
+      {isLoading ? (
+        <div className="loader">
+          <CircularProgress />
+        </div>
+      ) : (
+        <>
+          <div className="cards__container">
+            {cardData.map((card, index) => (
+              <div
+                key={card.id}
+                className={`cards__card ${activeCard === index ? 'active' : ''}`}
+                onClick={() => handleClick(index)}
+                style={{
+                  zIndex: cardData.length - index,
+                  transform:
+                    activeCard === index
+                      ? `translateY(calc(-100vh + 50px + 62.66vw + 25.06px))`
+                      : `translateY(${-index * 40}px)`,
+                }}
+              >
+                <Typography variant="cardText">BYN</Typography>
+                <Typography variant="bankName" className="cards__card__bank">
+                  {card.bank}
+                </Typography>
+                <br />
+                <Typography className="cards__card__number" variant="cardText">
+                  {card.number}
+                </Typography>
+                <Typography
+                  className="cards__card__expiration"
+                  variant="cardText"
+                >
+                  {card.expiration_date}
+                </Typography>
+              </div>
+            ))}
+          </div>
+          <div className="cards__add-card-button">
+            <Typography variant="plus">+</Typography>
+          </div>
+
           <div
-            key={card.id}
-            className={`cards__card ${activeCard === index ? 'active' : ''}`}
-            onClick={() => handleClick(index)}
+            className="cards__planning__button"
             style={{
-              zIndex: cardData.length - index,
-              transform:
-                activeCard === index
-                  ? `translateY(calc(-100vh + 50px + 62.66vw + 25.06px))`
-                  : `translateY(${-index * 40}px)`,
+              height: isOpen ? '60vh' : '55px',
+              borderRadius: isOpen ? '15px' : '15vw',
             }}
           >
-            <Typography variant="cardText">BYN</Typography>
-            <Typography variant="bankName" className="cards__card__bank">
-              {card.bank}
+            <Typography
+              variant="h5"
+              className="cards__planning__button__text"
+              onClick={() => setIsOpen(!isOpen)}
+            >
+              {isOpen ? `Перевод ▼` : `Перевод ▲`}
             </Typography>
-            <br />
-            <Typography className="cards__card__number" variant="cardText">
-              {card.number}
-            </Typography>
-            <Typography className="cards__card__expiration" variant="cardText">
-              {card.expiration_date}
-            </Typography>
+
+            <div class="cards__planning__container">
+              <Typography variant="category">С карты</Typography>
+              <br />
+              <Select
+                value={fromCard}
+                onChange={(e) => setFromCard(e.target.value)}
+                className="cards__planning__select"
+              >
+                {cardData.map((card) => (
+                  <MenuItem key={card.id} value={card.id}>
+                    {card.bank} | {card.number}
+                  </MenuItem>
+                ))}
+              </Select>
+              <br />
+              <Typography variant="category">На карту</Typography>
+              <br />
+              <Select
+                value={toCard}
+                onChange={(e) => setToCard(e.target.value)}
+                className="cards__planning__select"
+              >
+                {cardData.map((card) => (
+                  <MenuItem key={card.id} value={card.id}>
+                    {card.bank} | {card.number}
+                  </MenuItem>
+                ))}
+              </Select>
+
+              <br />
+              <Typography variant="category">Сумма</Typography>
+              <br />
+              <div className="cards__planning__flexbox">
+                <TextField
+                  type="number"
+                  value={amount}
+                  onChange={(e) => setAmount(e.target.value)}
+                  className="cards__planning__amount-input"
+                />
+                <Typography variant="category">BYN</Typography>
+              </div>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => setIsOpen(false)}
+                className="cards__planning__submit-button"
+              >
+                Перевести
+              </Button>
+            </div>
           </div>
-        ))}
-      </div>
-      <div className="cards__add-card-button">
-        <Typography variant="plus">+</Typography>
-      </div>
+        </>
+      )}
     </>
   );
 }
