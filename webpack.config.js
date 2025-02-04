e06066710b425382bd712935db6003608e773caf
kandr <kandrat.71@mail.ru>
2025-02-04 04:22:28 +0300

    Setting up webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const dotenv = require('dotenv'); 
+dotenv.config(); 
+const webpack = require('webpack'); 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { GenerateSW } = require('workbox-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
@@ -48,6 +51,9 @@ module.exports = (env) => {
       extensions: ['', '.js', '.jsx', '.scss'],
     },
     plugins: [
+      new webpack.DefinePlugin({ 
+        'process.env.REACT_APP_API': JSON.stringify(process.env.REACT_APP_API),
+      }),
       new CleanWebpackPlugin(),
       new HtmlWebpackPlugin({
         template: './public/index.html',
