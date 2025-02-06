849973c87856895ae14309225a94816bac27c641
ARTYOM MAKAROV <artemmakarovv05@gmail.com>
2025-02-06 19:29:09 +0300

    Update MobileMenuNav.jsx

--- src/components/mobileMenu/MobileMenuNav.jsx
+++ src/components/mobileMenu/MobileMenuNav.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
 import { ROUTES } from '@constants';
 
-function MobileMenuNav({}) {
+function MobileMenuNav() {
+  const location = useLocation();
+  const [activeItem, setActiveItem] = useState('');
+
+  useEffect(() => {
+    // Устанавливаем активный элемент в зависимости от текущего маршрута
+    const currentPath = location.pathname;
+    if (currentPath === ROUTES.CARDS.PATH) {
+      setActiveItem('cards');
+    } else if (currentPath === ROUTES.ANALITIC.PATH) {
+      setActiveItem('analitic');
+    } else if (currentPath === ROUTES.FAMILY.PATH) {
+      setActiveItem('family');
+    } else if (currentPath === ROUTES.PROFILE.PATH) {
+      setActiveItem('profile');
+    }
+  }, [location]);
+
+  const handleIconClick = (item) => {
+    setActiveItem(item);
+  };
+
   return (
     <nav className="mobile-menu__nav">
       <ul className="mobile-menu__nav__menu-list">
@@ -10,25 +31,44 @@ function MobileMenuNav({}) {
           <NavLink
             to={ROUTES.CARDS.PATH}
             className="mobile-menu__link mobile-menu__link--cards"
-          />
+            onClick={() => handleIconClick('cards')}
+            style={{ color: activeItem === 'cards' ? '#7268e5' : '#999999' }}
+          >
+            Карты
+          </NavLink>
         </li>
         <li className="mobile-menu__nav__item">
           <NavLink
             to={ROUTES.ANALITIC.PATH}
             className="mobile-menu__link mobile-menu__link--analitic"
-          />
+            onClick={() => handleIconClick('analitic')}
+            style={{ color: activeItem === 'analitic' ? '#7268e5' : '#999999' }}
+          >
+            Аналитика
+          </NavLink>
+        </li>
+        <li className="mobile-menu__nav__item">
+          <div className="icon-container"></div>
         </li>
         <li className="mobile-menu__nav__item">
           <NavLink
             to={ROUTES.FAMILY.PATH}
             className="mobile-menu__link mobile-menu__link--family"
-          />
+            onClick={() => handleIconClick('family')}
+            style={{ color: activeItem === 'family' ? '#7268e5' : '#999999' }}
+          >
+            Семья
+          </NavLink>
         </li>
         <li className="mobile-menu__nav__item">
           <NavLink
             to={ROUTES.PROFILE.PATH}
             className="mobile-menu__link mobile-menu__link--profile"
-          />
+            onClick={() => handleIconClick('profile')}
+            style={{ color: activeItem === 'profile' ? '#7268e5' : '#999999' }}
+          >
+            Профиль
+          </NavLink>
         </li>
       </ul>
     </nav>
