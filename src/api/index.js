a59d1fac30dbeac7aa1977ab211a6cc776d1b0d4
kandr <kandrat.71@mail.ru>
2025-03-06 14:08:08 +0300

    fixed tokens

--- src/api/index.js
+++ src/api/index.js
@@ -9,3 +9,14 @@ export const instance = axios.create({
   baseURL: REACT_APP_API + `/api`,
   withCredentials: true,
 })
+
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token && !config.url.includes('/auth/login')) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
