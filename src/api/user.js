7fb4e66ce3b5d723265ced6030ffed43b362fbda
ARTYOM MAKAROV <artemmakarovv05@gmail.com>
2025-03-10 19:45:13 +0300

    fix: balance display

--- src/api/user.js
+++ src/api/user.js
@@ -2,4 +2,7 @@ import { instance } from '.'
 
 export const userApi = {
   getProfile: () => instance.get(`/user/profile`).then((response) => response.data),
+
+  updateBalance: (body) => instance.patch('/users/balance', body),
+  getUserBalance: () => instance.get('/users/balance'),
 }
