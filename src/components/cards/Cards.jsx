22904091c8bb13e5b14faa63c98ebe3d05ceba82
ARTYOM MAKAROV <artemmakarovv05@gmail.com>
2025-03-13 14:28:53 +0300

    fix: cards(clsx, category, minor fixes), userapi path, icons

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,13 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Typography, IconButton } from '@mui/material'
 import clsx from 'clsx'
-import EditIcon from '@assets/imgs/edit_icon.png'
-import ExpensesIcon from '@assets/imgs/expenses_icon.png'
-import ExpensesActiveIcon from '@assets/imgs/expenses_active_icon.png'
-import IncomesIcon from '@assets/imgs/incomes_icon.png'
-import IncomesActiveIcon from '@assets/imgs/incomes_active_icon.png'
-import CalendarIcon from '@assets/imgs/calendar_icon.png'
-import PieChartIcon from '@assets/imgs/pie_chart_icon.png'
 import PieChart from '@components/pieChart/PieChart'
 import { categoryApi, userApi } from '@api'
 import { ICONS_MAP } from '@constants'
@@ -33,8 +26,8 @@ function Cards() {
   const [balance, setBalance] = useState(0)
   const [userExpenses, setUserExpenses] = useState(0)
   const [userIncomes, setUserIncomes] = useState(0)
-  const [Ecategories, setECategories] = useState([])
-  const [Icategories, setICategories] = useState([])
+  const [eCategories, setECategories] = useState([])
+  const [iCategories, setICategories] = useState([])
   const [showExpenses, setShowExpenses] = useState(true)
   const [showIncomes, setShowIncomes] = useState(false)
   const [currentType, setCurrentType] = useState([])
@@ -49,66 +42,52 @@ function Cards() {
       } catch (error) {}
     }
 
-    const fetchUserExpenses = async () => {
-      try {
-        const response = await categoryApi.getUserExpenses()
-        const totalExpenses = response.data.reduce((acc, expense) => acc + expense.totalIncome, 0)
-        setUserExpenses(totalExpenses.toFixed(2))
-      } catch (error) {}
-    }
-
-    const fetchUserIncomes = async () => {
-      try {
-        const response = await categoryApi.getUserIncomes()
-        const totalIncomes = response.data.reduce((acc, income) => acc + income.totalIncome, 0)
-        setUserIncomes(totalIncomes.toFixed(2))
-      } catch (error) {}
-    }
-
     const fetchECategories = async () => {
       try {
         const response = await categoryApi.getUserExpenses()
+        const totalExpenses = response.data.reduce((acc, expense) => acc + expense.totalIncome, 0)
         const fetchedCategories = response.data.map(
           ({ categoryName, categoryIconUrl, totalIncome }) => ({
             title: categoryName,
             icon: ICONS_MAP[categoryIconUrl] || ICONS_MAP['custom'],
-            amount: totalIncome.toFixed(2),
+            amount: totalIncome,
           })
         )
         setECategories(fetchedCategories)
+        setUserExpenses(totalExpenses.toFixed(2))
       } catch (error) {}
     }
 
     const fetchICategories = async () => {
       try {
         const response = await categoryApi.getUserIncomes()
+        const totalIncomes = response.data.reduce((acc, income) => acc + income.totalIncome, 0)
         const fetchedCategories = response.data.map(
           ({ categoryName, categoryIconUrl, totalIncome }) => ({
             title: categoryName,
             icon: ICONS_MAP[categoryIconUrl] || ICONS_MAP['custom'],
-            amount: totalIncome.toFixed(2),
+            amount: totalIncome,
           })
         )
         setICategories(fetchedCategories)
+        setUserIncomes(totalIncomes.toFixed(2))
       } catch (error) {}
     }
 
     fetchUserBalance()
-    fetchUserExpenses()
-    fetchUserIncomes()
     fetchECategories()
     fetchICategories()
   }, [])
 
   useEffect(() => {
     if (showExpenses) {
-      setCurrentType(Ecategories)
+      setCurrentType(eCategories)
       setCurrentAmount(userExpenses)
     } else if (showIncomes) {
-      setCurrentType(Icategories)
+      setCurrentType(iCategories)
       setCurrentAmount(userIncomes)
     }
-  }, [showExpenses, showIncomes, Ecategories, Icategories])
+  }, [showExpenses, showIncomes, eCategories, iCategories])
 
   const handlePieChartClick = () => {
     setIsPieChartVisible(!isPieChartVisible)
@@ -133,9 +112,11 @@ function Cards() {
   const handleBlur = () => {
     const numericValue = parseFloat(balance)
     if (!isNaN(numericValue)) {
+      const formattedBalance = numericValue.toFixed(2)
+      setBalance(formattedBalance)
       updateBalance(numericValue)
     } else {
-      setBalance(0)
+      setBalance('0.00')
     }
     setIsEditingBalance(false)
   }
@@ -170,7 +151,7 @@ function Cards() {
             </Typography>
             {!isEditingBalance && (
               <IconButton onClick={handleEditClick} className='edit-link'>
-                <img src={EditIcon} className='edit-icon' alt='Edit' />
+                <img src={ICONS_MAP['edit']} className='edit-icon' alt='Edit' />
               </IconButton>
             )}
           </div>
@@ -194,33 +175,33 @@ function Cards() {
       <div className='expenses-income-sum'>
         <IconButton
           onClick={handleShowExpenses}
-          className={`expenses-income-sum1 ${showExpenses ? 'active' : ''}`}
+          className={clsx('expenses-income-sum1', { active: showExpenses })}
         >
           <img
-            src={showExpenses ? ExpensesActiveIcon : ExpensesIcon}
+            src={showExpenses ? ICONS_MAP['expenses_active'] : ICONS_MAP['expenses']}
             className='expenses-incomes-icon'
             alt='Expenses'
           />
-          <Typography className={`expenses-button-text ${showExpenses ? 'active' : ''}`}>
+          <Typography className={clsx('expenses-button-text', { active: showExpenses })}>
             {userExpenses}
           </Typography>
-          <Typography className={`expenses-button-text opacity ${showExpenses ? 'active' : ''}`}>
+          <Typography className={clsx('expenses-button-text', 'opacity', { active: showExpenses })}>
             BYN
           </Typography>
         </IconButton>
         <IconButton
           onClick={handleShowIncomes}
-          className={`expenses-income-sum2 ${showIncomes ? 'active' : ''}`}
+          className={clsx('expenses-income-sum2', { active: showIncomes })}
         >
           <img
-            src={showIncomes ? IncomesActiveIcon : IncomesIcon}
+            src={showIncomes ? ICONS_MAP['incomes_active'] : ICONS_MAP['incomes']}
             className='expenses-incomes-icon'
             alt='Incomes'
           />
-          <Typography className={`incomes-button-text ${showIncomes ? 'active' : ''}`}>
+          <Typography className={clsx('incomes-button-text', { active: showIncomes })}>
             {userIncomes}
           </Typography>
-          <Typography className={`incomes-button-text opacity ${showIncomes ? 'active' : ''}`}>
+          <Typography className={clsx('incomes-button-text', 'opacity', { active: showIncomes })}>
             BYN
           </Typography>
         </IconButton>
@@ -229,7 +210,7 @@ function Cards() {
         <Typography className='page-title1'>Аналитика</Typography>
         <IconButton className='graphic-date-selector'>
           {selectedDate}
-          <img src={CalendarIcon} className='calender-icon' alt='Calendar' />
+          <img src={ICONS_MAP['calendar']} className='calender-icon' alt='Calendar' />
         </IconButton>
       </div>
 
@@ -237,7 +218,7 @@ function Cards() {
         <div className='analitic-graphic'>
           <div className='chart-type-container'>
             <IconButton className='pie-chart-icon' onClick={handlePieChartClick}>
-              <img src={PieChartIcon} className='pie-chart-icon' alt='Pie Chart' />
+              <img src={ICONS_MAP['pie_chart']} className='pie-chart-icon' alt='Pie Chart' />
             </IconButton>
           </div>
 
@@ -273,18 +254,18 @@ function Cards() {
             Расходы
           </Typography>
           <div className='categories-list'>
-            {Ecategories.map((category, index) => (
+            {eCategories.map(({ title, icon, amount }, index) => (
               <div key={index} className='category-item'>
                 <div className='category-header'>
-                  <img src={category.icon} alt='icon' className='category-icon' />
+                  <img src={icon} alt='icon' className='category-icon' />
                   <Typography variant='category' className='category-title'>
-                    {category.title}
+                    {title}
                   </Typography>
                 </div>
                 <div className='category-details'>
                   <span className='price-text'>BYN</span>
                   <Typography variant='category' className='price'>
-                    {category.amount}
+                    {amount.toFixed(2)}
                   </Typography>
                 </div>
               </div>
@@ -299,18 +280,18 @@ function Cards() {
             Доходы
           </Typography>
           <div className='categories-list'>
-            {Icategories.map((category, index) => (
+            {iCategories.map(({ title, icon, amount }, index) => (
               <div key={index} className='category-item'>
                 <div className='category-header'>
-                  <img src={category.icon} alt='icon' className='category-icon' />
+                  <img src={icon} alt='icon' className='category-icon' />
                   <Typography variant='category' className='category-title'>
-                    {category.title}
+                    {title}
                   </Typography>
                 </div>
                 <div className='category-details'>
                   <span className='price-text'>BYN</span>
                   <Typography variant='category' className='price'>
-                    {category.amount}
+                    {amount.toFixed(2)}
                   </Typography>
                 </div>
               </div>
