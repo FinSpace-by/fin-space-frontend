e9e8c1f692d3db3232409293615dfd593bf25679
ARTYOM MAKAROV <artemmakarovv05@gmail.com>
2025-03-13 14:44:08 +0300

    Update user.js

--- src/api/user.js
+++ src/api/user.js
@@ -1,10 +1,10 @@
 import { instance } from '.'
 
-const USERS_PATH = '/users/balance'
+const USERS_PATH = '/users'
 
 export const userApi = {
   getProfile: () => instance.get(`/user/profile`).then((response) => response.data),
 
-  updateBalance: (body) => instance.patch(USERS_PATH, body),
-  getUserBalance: () => instance.get(USERS_PATH),
+  updateBalance: (body) => instance.patch(USERS_PATH + '/balance', body),
+  getUserBalance: () => instance.get(USERS_PATH + '/balance'),
 }
