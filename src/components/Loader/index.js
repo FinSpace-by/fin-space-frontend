9a4cd8117f3f5ae7b3e2d3a4c9412ed8a82863af
ARTYOM MAKAROV <artemmakarovv05@gmail.com>
2025-04-09 22:56:34 +0300

    fix: loader-add comments

---
+++ src/components/Loader/index.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react'
+import { CircularProgress } from '@mui/material'
+import './sass/index.scss'
+
+function Loader({ isLoading }) {
+  const [isShow, setIsShow] = useState(false)
+
+  useEffect(() => {
+    if (isLoading) {
+      setIsShow(true)
+    } else {
+      const timer = setTimeout(() => setIsShow(false), 500)
+      return () => clearTimeout(timer)
+    }
+  }, [isLoading])
+
+  if (!isShow) return null
+
+  return (
+    <div className='loader-overlay'>
+      <CircularProgress size={80} thickness={6} />
+    </div>
+  )
+}
+
+export default Loader
