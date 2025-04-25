842158924d5429381de48080b63efab9878b5c08
kandr <kandrat.71@mail.ru>
2025-04-25 23:16:00 +0300

    fixed google

--- src/components/googleButton/GoogleButton.jsx
+++ src/components/googleButton/GoogleButton.jsx
@@ -1,31 +1,26 @@
 import React from 'react'
 import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google'
-
 import './sass/index.scss'
 
 const GoogleButton = ({ onSuccess, onError, clientId }) => {
-  const handleClick = () => {
-    const googleButton = document.querySelector('.google-auth-button__hidden div[role=button]')
-    if (googleButton) googleButton.click()
-  }
-
   return (
-    <div className='google-auth-button'>
-      <button type='button' className='google-auth-button__custom' onClick={handleClick}>
-        <img
-          src='https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg'
-          alt='Google'
-          className='google-auth-button__custom__icon'
+    <GoogleOAuthProvider clientId={clientId}>
+      <div className='google-auth-button'>
+        <GoogleLogin
+          onSuccess={onSuccess}
+          onError={onError}
+          render={({ onClick }) => (
+            <button type='button' className='google-auth-button__custom' onClick={onClick}>
+              <img
+                src='https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg'
+                alt='Google'
+                className='google-auth-button__custom__icon'
+              />
+            </button>
+          )}
         />
-        Войти через Google
-      </button>
-
-      <div className='google-auth-button__hidden'>
-        <GoogleOAuthProvider clientId={clientId}>
-          <GoogleLogin onSuccess={onSuccess} onError={onError} />
-        </GoogleOAuthProvider>
       </div>
-    </div>
+    </GoogleOAuthProvider>
   )
 }
 
